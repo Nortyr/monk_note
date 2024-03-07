2144806e83e64510bfd174390506cabd2daa0728
chenhui <321785796@qq.com>
2024-03-07 13:11:21 +0800

    20240307

---
+++ kafka_api/src/main/java/com/mountain/monk/KafkaConsumerService.java
@@ -0,0 +1,26 @@
+package com.mountain.monk;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+public class KafkaConsumerService {
+
+    @KafkaListener(topics = "myTopic", groupId = "myGroup")
+    public void listen(String message) {
+        System.out.println("Received message: " + message);
+        // 处理消息逻辑
+    }
+}
---
+++ kafka_api/src/main/java/com/mountain/monk/KafkaProducerService.java
@@ -0,0 +1,37 @@
+package com.mountain.monk;
+
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.support.SendResult;
+import org.springframework.stereotype.Service;
+import org.springframework.util.concurrent.ListenableFuture;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+public class KafkaProducerService {
+    private static final Logger log = LoggerFactory.getLogger(Consumer.class);
+
+    private final KafkaTemplate<String, String> kafkaTemplate;
+
+    public KafkaProducerService(KafkaTemplate<String, String> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    public void sendMessage(String topic, String message) {
+        ListenableFuture<SendResult<String, String>> future = kafkaTemplate.send(topic, message);
+        future.addCallback(success -> log.info("发送消息成功!"), failure -> log.error("发送消息失败!失败原因是:{}", failure.getMessage()));
+
+    }
+}
+
+
